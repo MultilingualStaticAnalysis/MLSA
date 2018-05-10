47e3bc997e26ef0e53ab90440e8041e866055e7b
MultilingualStaticAnalysis <multilingualsa@gmail.com>
2018-05-10 17:07:12 -0400

    Update cSA.py

--- bin/cSA.py
+++ bin/cSA.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
 # MLSA Multilingual Software Analysis
 # This program is part of the MLSA package under development at Fordham University Department of Computer and Information Science.
-# cSA.py takes in a C or C++ file and generates a text ast file from that C/C++ file. cFunCall is called with the ast file passed to it, and creates a csv file of all the call graphs in the C/C++ program. pyViaC.py is called with that csv file, and results in a new csv file where any Python files called in the C/C++ program are substituted in for the appropriate API. That resulting csv file is then returned.
+# cSA.py takes in a C or C++ file and generates a text ast file from that C/C++ file. cFunCall is called with the ast file passed to it,
+#       and creates a csv file of all the call graphs in the C/C++ program. pyViaC.py is called with that csv file, 
+#       and results in a new csv file where any Python files called in the C/C++ program are substituted in for the appropriate API. 
+#       That resulting csv file is then returned.
 # Author: Anne Marie Bogar
-# Date: June 13, 2017
+# Date: May 9, 2018
 # this code can be copied or used and is without warrenty or support, but this header needs to be copied along with the program FU2017
 # Input: Python file
 # Output: csv file
@@ -14,6 +18,7 @@
 import os
 import sys
 import pyViaC
+import cFunCall2
 import cRDAGroup
 
 #global variables
@@ -37,11 +42,10 @@ def main(cfile, funcfile):
     #generate ast
     os.system('clang-check-3.8 -ast-dump ' + cfile + ' --extra-arg="-fno-color-diagnostics" -- > ' + astfile)
 
-    #cFunCall generates a function call csv file (csvfile)
-    path = os.getcwd()+"/../bin/"
-    os.system("python "+path+"cFunCall.py "+astfile+" "+cfile+" "+csvfile+" "+funcfile)
+    # cFunCall2 generates a function call csv file (csvfile)
+    cFunCall2.main(astfile, cfile, csvfile, funcfile)
     #cRDA generates an rda csv file (rdafile)
-    cRDAGroup.main(astfile, cfile, rdafile)
+    #cRDAGroup.main(astfile, cfile, rdafile)
     #pyViaC generates another function call csv file but with Python file substitution
     pyViaC.main(csvfile, cfile, rdafile, finalCsv)
 
@@ -52,6 +56,7 @@ def main(cfile, funcfile):
     
     return finalCsv
 
+# **use for debugging purposes
 #if len(sys.argv) != 2:
 #    sys.exit(error+"2 arguments needed")
 
